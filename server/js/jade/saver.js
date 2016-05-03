944aca7f1dd2baad8be53da3b6b125be9a85b8a4
Mark Parson <mark@delorum.com>
2016-05-03 14:36:46 -0600

    Removed server from main repo

--- server/js/jade/saver.js
+++
@@ -1,7 +0,0 @@
-module.exports = function template(locals) {
-var buf = [];
-var jade_mixins = {};
-var jade_interp;
-
-buf.push("<div class=\"saver\"><div class=\"save-btn\">Save</div></div>");;return buf.join("");
-}
